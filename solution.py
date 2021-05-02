5a82c702dd7536cb647b13a0a207cf820ef2cbc4
jyshtty <56307643+jyshtty@users.noreply.github.com>
2021-05-02 20:33:34 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,15 @@
 
+stairs = 5
+steps = [1,2,3]
+
+def unique_ways(stairs, steps):
+        dp = [0] * (stairs + 1)
+        dp[0] = 1
+        for step in steps:
+            for stair in range(stairs+1):
+                if stair >= step:
+                    dp[stair] = dp[stair] + dp[stair - step]
+        return dp[stairs]
+
+
+print(unique_ways(stairs, steps))
